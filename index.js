e6b62099041842f16862ac639e05ec11d0db0e25
Dominic Cassidy <dominic@cassidy.casa>
2025-12-07 22:05:04 +0000

    more

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const { EdgeInsets, Color, Grow, BoxDecoration, Border } = require('./lib/base')
 const { Container, Text } = require('./lib/components')
+const { GrowRendererHTML } = require('./lib/html-renderer')
 const { GrowRendererTUI } = require('./lib/tui-renderer')
 
 const ui = new Container({
-  width: 200,
+  width: 1000,
   height: 100,
-  margin: EdgeInsets.all(5),
+  margin: EdgeInsets.all(10),
   decoration: new BoxDecoration({
     border: Border.all()
   }),
@@ -17,17 +18,37 @@ const ui = new Container({
     new Text({
       value: 'Hello again!',
       color: Color.from({ blue: 255 })
+    }),
+    new Container({
+      width: 500,
+      height: 30,
+      decoration: new BoxDecoration({
+        border: Border.all({ color: Color.from({ red: 255 }) })
+      }),
+      children: [
+        new Text({
+          value: 'Inner',
+          color: Color.from({ green: 255 })
+        })
+      ]
     })
   ]
 })
 
-ui.setAttribute('width', 250)
+// ui.setAttribute('width', 250)
 
 const grow = new Grow({
   renderer: new GrowRendererTUI(),
   child: ui
 })
 
-console.log(ui.toString())
+// console.log(ui.toString())
 
-console.log(grow.render())
+grow.render()
+
+const html = new Grow({
+  renderer: new GrowRendererHTML(),
+  child: ui
+})
+
+console.log(html.render())
