2412d57942e983fa5e6a9742313a468fa4df9247
Dominic Cassidy <dominic@cassidy.casa>
2025-12-09 00:58:38 +0000

    relative size and improvements

--- index.js
+++ index.js
@@ -1,40 +1,58 @@
 const process = require('process')
 
-const { EdgeInsets, Color, Grow, BoxDecoration, Border } = require('./lib/base')
-const { Container, Text, Center } = require('./lib/components')
+const { EdgeInsets, Color, Grow, BoxDecoration, Border, Alignment } = require('./lib/base')
+const { Container, Text, Center, TextAlign } = require('./lib/components')
 const { GrowRendererHTML } = require('./lib/html-renderer')
 const { GrowRendererTUI } = require('./lib/tui-renderer')
 
 const ui = new Container({
-  width: 1000,
-  height: 100,
+  width: '100%',
+  height: '100%',
   margin: EdgeInsets.all(2),
+  color: Color.from('#000'),
   decoration: new BoxDecoration({
     border: Border.all()
   }),
   children: [
+    new Container({
+      width: '100%',
+      height: 3,
+      decoration: new BoxDecoration({
+        border: Border.all({ color: Color.from('#bade5b') })
+      }),
+      children: [
+        new Center({
+          width: '100%',
+          height: 3,
+          child: new Text({
+            value: 'My First Repo',
+            color: Color.from('#fff')
+          })
+        })
+      ]
+    }),
     new Text({
       value: 'Hello world!',
       color: Color.from({ red: 255 })
     }),
     new Text({
       value: 'Hello again!',
+      textAlign: TextAlign.Right,
       color: Color.from({ blue: 255 })
     }),
-    new Center({
-      child: new Container({
-        width: process.stdout.columns / 2,
-        height: 3,
-        decoration: new BoxDecoration({
-          border: Border.all({ color: Color.from({ red: 255 }) })
-        }),
-        children: [
-          new Text({
-            value: 'Inner',
-            color: Color.from({ green: 255 })
-          })
-        ]
-      })
+    new Container({
+      width: '50%',
+      height: 3,
+      alignment: Alignment.Center,
+      decoration: new BoxDecoration({
+        border: Border.all({ color: Color.from('#bade5b') })
+      }),
+      children: [
+        new Text({
+          value: 'Inner',
+          color: Color.from({ green: 255 })
+        })
+      ]
     })
   ]
 })
@@ -44,9 +62,6 @@ const ui = new Container({
     renderer: new GrowRendererTUI(),
     child: ui
   })
-
-  ui.setAttribute('width', process.stdout.columns)
-  ui.setAttribute('height', process.stdout.rows)
 
   // TODO: raw mode?
   // process.stdout.on('resize', () => {
@@ -58,14 +73,14 @@ const ui = new Container({
   grow.render()
 }
 
-{
-  const grow = new Grow({
-    renderer: new GrowRendererHTML(),
-    child: ui
-  })
+// {
+//   const grow = new Grow({
+//     renderer: new GrowRendererHTML(),
+//     child: ui
+//   })
 
-  ui.setAttribute('width', 720)
-  ui.setAttribute('height', 480)
+//   ui.setAttribute('width', 720)
+//   ui.setAttribute('height', 480)
 
-  console.log(grow.render())
-}
+//   console.log(grow.render())
+// }
