78278081808af39db49e8947bbf9d1e785922f5d
Dominic Cassidy <dominic@cassidy.casa>
2025-12-17 00:19:41 +0000

    fix resize on re-render

--- index.js
+++ index.js
@@ -3,65 +3,73 @@ const { Container, Text, Center, TextAlign, Pressable } = require('./lib/compone
 // const { GrowRendererHTML } = require('./lib/html-renderer')
 const { GrowRendererTUI } = require('./lib/tui-renderer')
 
-const ui = new Container({
-  width: '100%',
-  height: '100%',
-  margin: EdgeInsets.all(2),
-  alignment: Alignment.Center,
-  decoration: new BoxDecoration({
-    border: Border.all()
-  }),
-  children: [
-    new Pressable({
-      onPress: () => {},
-      hotkey: new HotKey({ key: '1' }),
-      child: new Container({
-        width: '100%',
+function App(props = {}) {
+  let selected = props.selected || null
+
+  return new Container({
+    width: '100%',
+    height: '100%',
+    margin: EdgeInsets.all(2),
+    alignment: Alignment.Center,
+    decoration: new BoxDecoration({
+      border: Border.all()
+    }),
+    children: [
+      new Pressable({
+        onPress: function () {
+          this.renderer.render(App({ selected: '1' }))
+        },
+        hotkey: new HotKey({ key: '1' }),
+        child: new Container({
+          width: '100%',
+          height: 3,
+          decoration: new BoxDecoration({
+            border: Border.all({
+              color: selected === '1' ? Color.from('#f00') : Color.from('#bade5b')
+            })
+          }),
+          children: [
+            new Center({
+              width: '100%',
+              height: 3,
+              child: new Text({
+                value: 'My First Repo',
+                color: Color.from('#fff')
+              })
+            })
+          ]
+        })
+      }),
+      new Text({
+        value: 'Hello world!',
+        color: Color.from({ red: 255 })
+      }),
+      new Text({
+        value: 'Hello again!',
+        textAlign: TextAlign.Right,
+        color: Color.from({ blue: 255 })
+      }),
+      new Container({
+        width: '50%',
         height: 3,
         decoration: new BoxDecoration({
           border: Border.all({ color: Color.from('#bade5b') })
         }),
         children: [
-          new Center({
-            width: '100%',
-            height: 3,
-            child: new Text({
-              value: 'My First Repo',
-              color: Color.from('#fff')
-            })
+          new Text({
+            value: 'Inner',
+            color: Color.from('#fff')
           })
         ]
       })
-    }),
-    new Text({
-      value: 'Hello world!',
-      color: Color.from({ red: 255 })
-    }),
-    new Text({
-      value: 'Hello again!',
-      textAlign: TextAlign.Right,
-      color: Color.from({ blue: 255 })
-    }),
-    new Container({
-      width: '50%',
-      height: 3,
-      decoration: new BoxDecoration({
-        border: Border.all({ color: Color.from('#bade5b') })
-      }),
-      children: [
-        new Text({
-          value: 'Inner',
-          color: Color.from('#fff')
-        })
-      ]
-    })
-  ]
-})
+    ]
+  })
+}
 
 {
   const grow = new Grow({
     renderer: new GrowRendererTUI(),
-    child: ui
+    child: App()
   })
 
   // TODO: auto resize?
